c8ba7eaa7ca3009ec78ef8ce0e3c6c51c89c4da2
unknown <swapnilkuthe20@gmail.com>
2025-03-24 10:01:07 +0530

    Update shit

--- demo.js
+++ demo.js
@@ -1,7 +1,81 @@
+// Print even nos from 1 to 10
 
-let arr = [11, 12, 15, 36, 53, 14, 27];
-let arr2 = [21, 23, 24, 25]
+// let i = 1;
 
-//  push , pop, shift, unshift, slice, splice, sort, reverse, indexOf, lastIndexOf,includes, join, concate, toString()
+// while (i <= 10) {
+//     if (i % 2 == 0) {
+//         // console.log(i);
+//     }
+//     i++;
+// }
+
+
+// Ask for user Input untill they enter valid name 
+
+// let name = "";
+
+// while (!name) {
+//     name = prompt("Enter Name")
+// }
+
+// console.log("hello ", name);
+
+//  Sum of numbers from 1-10;;;;;;
+
+
+// let total = 0;
+// let num = 1;
+// while (num <= 10) {
+//     total += num;
+//     num++
+// }
+// console.log("total", total);
+
+
+
+// CountDown from 10 to 0
+
+// let i = 10;
+
+// while (i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+
+
+//  Generate random numbers until we get 5 ::
+
+// let random;
+// while (random != 5) {
+//     random = Math.ceil(Math.random() * 10)
+//     // console.log(random);
+// }
+
+// Write a do...while loop that prints even numbers from 2 to 20.
+
+let even = 0;
+let i = 2;
+do {
+    if (i % 2 == 0) {
+        even = i;
+        console.log(even);
+    }
+    i++;
+} while (i <= 20)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
